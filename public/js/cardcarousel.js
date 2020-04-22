95a1e448278088b47a6e1adfbde90da6fd3c34d7
Alex Bicknell <alexander.bicknell@gmail.com>
2020-04-22 19:21:21 +1000

    Front-End changes
    Completed home
    Adding feedback, course and course details page

---
+++ public/js/cardcarousel.js
@@ -0,0 +1,26 @@
+//Known issues: Slide breaks at the end - repeats the first carousel item after first cycle
+
+$(document).ready(function() {
+  $("#myCarousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $("#myCarousel .carousel-item ").length  //#myCarousel 
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
